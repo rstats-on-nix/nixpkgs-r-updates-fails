f0c4b43993996ea5d1a882a057fb93e78ac0b346
Bruno Rodrigues <bruno.rodrigues@mesr.etat.lu>
2024-02-20 14:32:59 +0100

    first commit

---
+++ _targets.R
@@ -0,0 +1,89 @@
+library(targets)
+library(tarchetypes)
+
+tar_option_set(packages = c(
+                 "rvest"
+               ))
+
+safe_packageRank <- function(...){
+  tryCatch(
+    packageRank::packageRank(...),
+    error = function(e) NULL
+  )
+}
+
+
+list(
+  tar_target(
+    evaluations_url,
+    "https://hydra.nixos.org/jobset/nixpkgs/r-updates#tabs-evaluations"
+  ),
+
+  tar_target(
+    evaluations_tables,
+    read_html(evaluations_url)
+  ),
+
+  tar_target(
+    evaluations,
+    {
+      html_table(html_nodes(evaluations_tables, "[class = 'table table-condensed table-striped clickable-rows']"))[[1]] |>
+        subset(Date != "Date", select = `#`)
+    }
+  ),
+
+  tar_target(
+    last_evaluation,
+    evaluations[1, "#"]
+  ),
+
+  tar_target(
+    last_jobset_url,
+    paste0("https://hydra.nixos.org/eval/", last_evaluation, "#tabs-still-fail")
+  ),
+
+  tar_target(
+    failing_jobs_html,
+    read_html(last_jobset_url)
+  ),
+
+
+  tar_target(
+    failing_jobs_raw,
+    html_table(html_nodes(failing_jobs_html, "[id = 'tabs-still-fail']"))[[1]]
+  ),
+
+  tar_target(
+    failing_jobs,
+    subset(failing_jobs_raw,
+           subset =!grepl("builds omitted", System),
+           select = c("#", "Finished at", "Package/release name", "System")) |>
+    transform(packages = gsub("^r-", "", `Package/release name`)) |>
+    transform(packages = gsub("-.*$", "", packages)) |>
+    transform(build = paste0("https://hydra.nixos.org/build/", X.)) |>
+    transform(build = paste0('<a href="', build, '">"', build, '</a>'))
+  ),
+
+  tar_target(
+    latest_eval_date,
+    max(failing_jobs$`Finished.at`)
+  ),
+
+  tar_target(
+    unique_packages,
+    unique(failing_jobs$packages)
+  ),
+
+  tar_target(
+    results_table,
+    safe_packageRank(packages = unique_packages[1:5])
+  ),
+
+  tar_force(
+    name = paper,
+    command = quarto::quarto_render("r-updates-fails.qmd"),
+    format = "file",
+    force = TRUE
+  )
+
+)
---
+++ default.nix
@@ -0,0 +1,26 @@
+let
+ pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/06b93631a20bc9c1e73d7b5c706af12ee01922aa.tar.gz") {};
+ system_packages = builtins.attrValues {
+  inherit (pkgs) R glibcLocalesUtf8 quarto nix;
+};
+  in
+  pkgs.mkShell {
+    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+
+    buildInputs = [
+      system_packages
+      pkgs.rPackages.quarto
+      pkgs.rPackages.reactable
+      pkgs.rPackages.rvest
+      pkgs.rPackages.tarchetypes
+      pkgs.rPackages.targets
+                  ];
+
+    shellHook = '' Rscript -e "targets::tar_make()" '';
+  }
