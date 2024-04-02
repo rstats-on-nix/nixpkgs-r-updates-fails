7ff8ab466303b55b1ca16e2b93c65a97233778e1
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-04-02 11:37:09 +0200

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -155,7 +155,7 @@ list(
   tar_target(
     prs_df,
     rbind(open_prs, merged_prs) |>
-    subset(#subset = PR_date >= latest_eval_date,
+    subset(subset = state != "merged",
            select = c("fails_because_of", "PR", "PR_date", "state")
            )
   ),
